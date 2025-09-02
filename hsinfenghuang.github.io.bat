1cb055db3da1e20243f4e5b9fcdead1b6601769c
goldshoot0720 <goldshoot0720@gmail.com>
2025-09-03 01:32:22 +0800

    20250903 v1

--- hsinfenghuang.github.io.bat
+++ hsinfenghuang.github.io.bat
@@ -1,7 +1,7 @@
 cd "C:\Users\chbon\Documents\herokuhexohsinfenghuang\public"
 git init
 git add .
-git commit -m "20250822 v6"
+git commit -m "20250903 v1"
 git remote add origin https://github.com/hsinfenghuang/hsinfenghuang.github.io.git
 git checkout -b main
 git push -u origin main --force
