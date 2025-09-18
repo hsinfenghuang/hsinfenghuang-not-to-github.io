04898d0831b93f21e615bedc3d93f1bf045ea266
goldshoot0720 <goldshoot0720@gmail.com>
2025-09-19 00:46:07 +0800

    20250919 v1

--- hsinfenghuang-not-to-github.io.bat
+++ hsinfenghuang-not-to-github.io.bat
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-cd "C:\Users\chbon\Documents\hsinfenghuang-not-to-github.io-main"
-git init
-git add .
-git commit -m "20250903 v1"
-git remote add origin https://github.com/hsinfenghuang/hsinfenghuang-not-to-github.io.git
-git checkout -b main
-git push -u origin main
-=======
 cd "C:\Users\chbon\Downloads\hsinfenghuang-not-to-github.io-main"
 git init
 git add .
@@ -19,4 +10,3 @@ git status
 pause
 git push -u origin main
 pause
->>>>>>> b2ba47e (20250919 v1)
